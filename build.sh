2d97c457000c335553e751afe1372acfeeb63c4b
Frederick Shier <fred@red7mobile.com>
2018-07-23 13:43:03 +0100

    update spec

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 # Update OpSec
 
 # Update Master
+
+aws cloudformation update-stack --stack-name SB-Foundations --template-body file://master.yml --parameters file://parameters.json --capabilities CAPABILITY_NAMED_IAM
